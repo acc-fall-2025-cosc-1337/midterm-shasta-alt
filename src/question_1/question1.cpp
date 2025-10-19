fc8bbe9fc3c0e0e2ad6dd9e377e34b7385c6616b
shasta-alt <shasta.khatiwada@g.austincc.edu>
2025-10-18 19:27:51 -0500

    Add function to generate Fibonacci sequence
    Implemented Fibonacci sequence generator function.

--- src/question_1/question1.cpp
+++ src/question_1/question1.cpp
@@ -1,6 +1,20 @@
-#include "question1.h"
+#include "question_1.h"
+#include <sstream>
 
-bool test_config()
+// Returns Fibonacci sequence up to n terms
+// Example get_fib_sequence(5) -> "0 1 1 2 3 5"
+std::string get_fib_sequence(int n)
 {
-    return true;
+    if (n < 0) return "";
+    long long a = 0, b = 1;
+    std::ostringstream oss;
+    oss << a;
+    for (int i = 1; i <= n; ++i)
+    {
+        oss << " " << b;
+        long long next = a + b;
+        a = b;
+        b = next;
+    }
+    return oss.str();
 }
