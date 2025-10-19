9ff52f6203e5b887a9cdc191dbe8e68a7ad6aa8b
shasta-alt <shasta.khatiwada@g.austincc.edu>
2025-10-18 19:51:36 -0500

    Add find_gcd function for calculating GCD
    Implement GCD function to find the greatest common divisor of two numbers, with input validation.

--- src/question_3/question3.cpp
+++ src/question_3/question3.cpp
@@ -1,6 +1,22 @@
 #include "question3.h"
 
-bool test_config()
+int find_gcd(int num1, int num2)
 {
-    return true;
+    if (num1 <= 0 || num2 <= 0)
+        return -1;  // invalid inputs
+
+    int gcd = 1;
+    int smaller = (num1 < num2) ? num1 : num2;
+
+    for (int i = 1; i <= smaller; i++)
+    {
+        if (num1 % i == 0 && num2 % i == 0)
+        {
+            gcd = i;
+        }
+    }
+
+    if (gcd == 1 && num1 != num2)
+        return -1; // no common divisor except 1
+    return gcd;
 }
