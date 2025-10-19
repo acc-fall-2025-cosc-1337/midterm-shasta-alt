d5042164610e3494182e7183aa7f910c5be59572
shasta-alt <shasta.khatiwada@g.austincc.edu>
2025-10-18 19:48:15 -0500

    Add header guards and prime number functions

--- src/question_2/question2.h
+++ src/question_2/question2.h
@@ -1,1 +1,13 @@
-bool test_config();
+#ifndef QUESTION_2_H
+#define QUESTION_2_H
+
+#include <vector>
+using namespace std;
+
+// Function to check if a number is prime
+bool is_prime(int num);
+
+// Function to return all prime numbers up to num
+vector<int> get_primes(int num);
+
+#endif
