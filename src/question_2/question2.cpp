09ac73933d7458cae99fbdcd636919481d3ba503
shasta-alt <shasta.khatiwada@g.austincc.edu>
2025-10-18 19:48:35 -0500

    Implement prime number checking and retrieval functions
    Added functions to check for prime numbers and retrieve a list of primes up to a given number.

--- src/question_2/question2.cpp
+++ src/question_2/question2.cpp
@@ -1,6 +1,32 @@
-#include "question2.h"
+#include "question_2.h"
+#include <cmath>
 
-bool test_config()
+bool is_prime(int num)
 {
+    if (num <= 1)
+        return false;
+    if (num == 2)
+        return true;
+    if (num % 2 == 0)
+        return false;
+
+    for (int i = 3; i <= sqrt(num); i += 2)
+    {
+        if (num % i == 0)
+            return false;
+    }
     return true;
+}
+
+vector<int> get_primes(int num)
+{
+    vector<int> primes;
+    for (int i = 2; i <= num; ++i)
+    {
+        if (is_prime(i))
+        {
+            primes.push_back(i);
+        }
+    }
+    return primes;
 }
