8a40175c4846a693674d47db362d00c5a63da57c
shasta-alt <shasta.khatiwada@g.austincc.edu>
2025-10-18 19:57:45 -0500

    Add get_factorial_sequence function
    Implement get_factorial_sequence function to return the factorial expression for a given number.

--- src/question_4/question4.cpp
+++ src/question_4/question4.cpp
@@ -1,5 +1,21 @@
-//
-bool test_config()
+#include "question_4.h"
+
+string get_factorial_sequence(int num)
 {
-    return true;
+    if (num < 1)
+        return "Invalid";
+
+    long long result = 1;
+    string expression = "";
+
+    for (int i = 1; i <= num; ++i)
+    {
+        result *= i;
+        expression += to_string(i);
+        if (i < num)
+            expression += "x";
+    }
+
+    expression += "=" + to_string(result);
+    return expression;
 }
